9f33f69301c1667d4574493fff82c5593ce3dda1
Noel Schenk CGM <noel.schenk@cgm.com>
2020-03-21 07:17:38 +0100

    added routing and home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -11,14 +11,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { RegistrationComponent } from './forms/registration/registration.component';
 import { PageComponent } from './page/page.component';
 import { ContentComponent } from './content/content.component';
+import { StartComponent } from './start/start.component';
+
+const appRoutes: Routes = [
+  { path: '', component: StartComponent },
+  { path: 'registration', component: RegistrationComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, RegistrationComponent, PageComponent, ContentComponent],
+  declarations: [AppComponent, RegistrationComponent, PageComponent, ContentComponent, StartComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     StatusBar,
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[vvv-start]',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.scss'],
+})
+export class StartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
