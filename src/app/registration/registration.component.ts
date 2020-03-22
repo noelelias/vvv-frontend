9901cfaf1dfda554318f243a18b0856cf51772ba
Noel Schenk CGM <noel.schenk@cgm.com>
2020-03-22 12:42:45 +0100

    registration changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,37 +1,46 @@
-import {Component, OnInit, Input, Output} from '@angular/core';
-import {Router} from '@angular/router';
-import {ApiService} from '../services/ApiService';
-import {UserService} from '../services/UserService';
-import {User} from '../models/User';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/ApiService';
+import { UserService } from '../services/UserService';
+import { User } from '../models/User';
+import { toastController } from '@ionic/core';
 
 @Component({
-  selector: '[vvv-registration]',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+    selector: '[vvv-registration]',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  profileType = 'helper';
-  email = '';
-  password = '';
+    form = {
+        email: '',
+        password: '',
+        profileType: 'helper',
+        tos: false,
+        privacy: false
+    };
 
-  constructor(private router: Router, private api: ApiService, private userService: UserService) {
-  }
+    constructor(private router: Router, private api: ApiService, private userService: UserService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  segmentChanged() {
-    console.log(this.profileType);
-  }
+    validate() {
+        if (this.form.tos && this.form.privacy) return true;
+        else {
+			toastController.create({message: 'Bitte akzeptieren Sie die AGB\'s und Datenschutzbestimmungen'}).then(toast=>toast.present());
+            return false;
+        }
+    }
 
-  createProfile() {
-    this.api.register(this.email, this.password).subscribe((user: User) => {
-      this.userService.current = user;
-      if (this.profileType !== 'helper') {
-        this.router.navigate(['/registration/hospital']);
-      } else {
-        this.router.navigate(['/registration/helper']);
-      }
-    });
-  }
+    createProfile() {
+        if (!this.validate()) return;
+
+        this.api.register(this.form.email, this.form.password).subscribe((user: User) => {
+            this.userService.current = user;
+            if (this.form.profileType !== 'helper') {
+                this.router.navigate(['/registration/hospital']);
+            } else {
+                this.router.navigate(['/registration/helper']);
+            }
+        });
+    }
 }
--- src/app/services/ApiService.ts
+++ src/app/services/ApiService.ts
@@ -1,29 +1,38 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { ToastController } from '@ionic/angular';
+import { toastController } from '@ionic/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
+    constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-  }
+    get(url: string) {
+        return this.http.get(environment.apiBase + url);
+    }
 
-  get(url: string) {
-    return this.http.get(environment.apiBase + url);
-  }
+    post(url: string, body: any) {
+		const returnObservable = this.http.post(environment.apiBase + url, body).pipe(catchError( (err, caght) => {
+				toastController.create({message: err?.error?.messages?.join(' - ')}).then(toast=>toast.present());
+				return throwError(err);
+			}
+		));
+        returnObservable.subscribe(data => {
+            console.log(data);
+        });
+        return returnObservable;
+    }
 
-  post(url: string, body: any) {
-    return this.http.post(environment.apiBase + url, body);
-  }
+    login(email: string, password: string) {
+        return this.post('endpoints/auth/login.php', { email, pass: password });
+    }
 
-  login(email: string, password: string) {
-    return this.post('endpoints/auth/login.php', {email, pass: password});
-  }
-
-  register(email: string, password: string) {
-    return this.post('endpoints/auth/register.php', {email, pass: password});
-  }
-
+    register(email: string, password: string) {
+        return this.post('endpoints/auth/register.php', { email, pass: password });
+    }
 }
